ed00452b00beb0a3528efd43d42c521f82dd8d4b
Kanbagoly <kanbagoly@gmail.com>
2020-05-16 22:07:01 +0200

    Reuse tests between algorithms

--- src/test/scala/com/kanbagoly/pearls/Pearl1Spec.scala
+++ src/test/scala/com/kanbagoly/pearls/Pearl1Spec.scala
@@ -3,24 +3,36 @@ package com.kanbagoly.pearls
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class Pearl1Spec extends AnyWordSpec with Matchers {
+class Pearl1Spec extends AnyWordSpec with Matchers with MinFreeBehavior {
 
-  "Naive minFree" should {
+  "Naive algorithm" should {
+    behave like minFreeAlgorithm(Naive.minFree)
+  }
+
+  "Array based algorithm" should {
+    behave like minFreeAlgorithm(ArrayBased.minFree)
+  }
+
+}
+
+trait MinFreeBehavior {
+  this: AnyWordSpec with Matchers =>
+
+  def minFreeAlgorithm(algorithm: List[Int] => Int) {
     "return first element" when {
       "called with empty list" in {
-        Naive.minFree(Nil) should be(0)
+        algorithm(Nil) should be(0)
       }
       "called with 0" in {
-        Naive.minFree(List(0)) should be(1)
+        algorithm(List(0)) should be(1)
       }
       "called with 1" in {
-        Naive.minFree(List(1)) should be(0)
+        algorithm(List(1)) should be(0)
       }
-      "called with an example from the book" in {
+      "called with the example from the book" in {
         val example = List(8, 23, 9, 0, 12, 11, 1, 10, 13, 7, 41, 4, 14, 21, 5, 17, 3, 19, 2, 6)
-        Naive.minFree(example) should be (15)
+        algorithm(example) should be(15)
       }
     }
   }
-
 }
