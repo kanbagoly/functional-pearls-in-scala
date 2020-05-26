d68853bafe0204acf257e3d70b41cbbc8983a1af
Kanbagoly <kanbagoly@gmail.com>
2020-05-26 23:12:10 +0200

    Add working solution

--- src/main/scala/com/kanbagoly/pearls/pearl2/Pearl2.scala
+++ src/main/scala/com/kanbagoly/pearls/pearl2/Pearl2.scala
@@ -12,9 +12,27 @@ object Specification {
 
 object DivideAndConquer {
 
-  def msc[A](xs: List[A])(implicit ordered: A => Ordered[A]): Int =
-    table(xs).map(_._2).max
+  //TODO: Deviation from the book
+  def msc[A](xs: List[A])(implicit ordered: A => Ordered[A]): Int = xs match {
+    case Nil => 0
+    case xs => table(xs).map(_._2).max
+  }
 
-  private def table[A](xs: List[A])(implicit ordered: A => Ordered[A]): List[(A, Int)] = ???
+  private def table[A](xs: List[A])(implicit ordered: A => Ordered[A]): List[(A, Int)] = xs match {
+    case x::Nil => List((x, 0))
+    case xs =>
+      val m = xs.length
+      val n = m / 2
+      val (ys, zs) = xs splitAt n
+      join(m - n, table(ys), table(zs))
+  }
+
+  private def join[A](n: Int, txs: List[(A, Int)], tys: List[(A, Int)])(implicit ordered: A => Ordered[A]): List[(A, Int)] =
+    (n, txs, tys) match {
+      case (0, txs, Nil) => txs
+      case (_, Nil, tys) => tys
+      case (n, (x, c)::txs1, (y, _)::_) if x < y => (x , c + n) :: join(n, txs1, tys)
+      case (n, _, (y, d)::tys1) => (y, d) :: join(n - 1, txs, tys1)
+    }
 
 }
