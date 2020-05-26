d1653a686b59db156bf54138bc5eb4e1b4959f4f
Kanbagoly <kanbagoly@gmail.com>
2020-05-27 01:43:22 +0200

    Add better testname

--- src/test/scala/com/kanbagoly/pearls/pearl2/Pearl2Spec.scala
+++ src/test/scala/com/kanbagoly/pearls/pearl2/Pearl2Spec.scala
@@ -12,8 +12,10 @@ class Pearl2Spec extends AnyWordSpec with Matchers with MaximumSurpasserCountBeh
   "Divide and Conquer algorithm" should {
     behave like maxSurpasserCountAlgorithm(DivideAndConquer.msc)
 
-    "able to work with large input" in {
-      DivideAndConquer.msc(List.fill(1000000)('a')) should be(0)
+    "not throw stack overflow exception" when {
+      "executed with large input" in {
+        DivideAndConquer.msc(List.fill(100000)('a')) should be(0)
+      }
     }
   }
 
