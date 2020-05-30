d5fb91c5757ed800766618f00fa12227e6ee8449
Kanbagoly <kanbagoly@gmail.com>
2020-05-30 23:05:29 +0200

    Make specification tail recursive

--- src/main/scala/com/kanbagoly/pearls/pearl4/Pearl4.scala
+++ src/main/scala/com/kanbagoly/pearls/pearl4/Pearl4.scala
@@ -1,14 +1,15 @@
 package com.kanbagoly.pearls.pearl4
 
+import scala.annotation.tailrec
+
 object Pearl4 {
 
-  // TODO: be tail recursive
   def smallest[A](k: Int, xs: List[A], ys: List[A])(implicit ordered: A => Ordered[A]): A = {
-    def union(xs: List[A], ys: List[A]): List[A] = (xs, ys) match {
-      case (xs, Nil) => xs
-      case (Nil, ys) => ys
-      case (x::xs, y::_) if x < y => x :: union(xs, ys)
-      case (_, y::ys) => y :: union(xs, ys)
+    @tailrec def union(xs: List[A], ys: List[A], acc: List[A] = Nil): List[A] = (xs, ys) match {
+      case (xs, Nil) => acc.reverse:::xs
+      case (Nil, ys) => acc.reverse:::ys
+      case (x::xs, y::_) if x < y => union(xs, ys, x::acc)
+      case (_, y::ys) => union(xs, ys, y::acc)
     }
     union(xs, ys)(k)
   }
