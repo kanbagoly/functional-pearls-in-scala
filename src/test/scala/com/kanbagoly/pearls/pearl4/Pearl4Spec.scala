efd646e362ed7dceb3d51d7a41ff2042ec4cc335
Kanbagoly <kanbagoly@gmail.com>
2020-05-30 22:56:46 +0200

    Add more test

--- src/test/scala/com/kanbagoly/pearls/pearl4/Pearl4Spec.scala
+++ src/test/scala/com/kanbagoly/pearls/pearl4/Pearl4Spec.scala
@@ -11,6 +11,16 @@ class Pearl4Spec extends AnyWordSpec with Matchers {
         Pearl4.smallest(0, Nil, List(1)) should be(1)
       }
     }
+    "First collection contain only one element and and second is empty" should {
+      "return the one element" in {
+        Pearl4.smallest(0, List(1), Nil) should be(1)
+      }
+    }
+    "Both collection contains many elements" should {
+      "return the k th element" in {
+        Pearl4.smallest(10, (0 to 20 by 2).toList, (1 to 21 by 2).toList) should be(10)
+      }
+    }
   }
 
 }
